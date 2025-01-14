8628952e89fb18bd774983dadda440bf8db75d15
stiven lovera <stivenlovera@gmail.com>
2025-01-14 17:06:00 -0400

    correcion de bucle

--- src/run/run-artista.ts
+++ src/run/run-artista.ts
@@ -24,7 +24,7 @@ export async function inizialize() {
     const mongoDB = await AppDataSource.initialize();
     const obras = await AppDataSourceMysql.query<getAllLibro[]>(`
            select * from (select dato.pagina_id, pagina.nombre as pagina_nombre, libro.dato_id, dato.nombre, libro.libro_id, libro.completed, libro.href, GROUP_CONCAT( libro.libro_id SEPARATOR ',') as ids_libros, GROUP_CONCAT(libro.href  SEPARATOR ',') as links, count( DISTINCT libro.libro_id ) as cantidad from dato inner join libro on libro.dato_id=dato.dato_id inner join pagina on pagina.pagina_id=dato.pagina_id group by libro.href
-) as libro where libro.completed=0 and libro.pagina_id=${pagina} and libro.libro_id=4250
+) as libro where libro.completed=0 and libro.pagina_id=${pagina}
         `);
     logger.info(`artistas almacenado en base de datos :${convertJson(obras.length)}`)
 
